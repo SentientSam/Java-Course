d4c02187525df3953a0e127ad63eb6df7c52fb84
SentientSam <samueltlamb1997@gmail.com>
2020-04-16 12:52:02 -0500

    Add files via upload
    Takes the square root of a variable, number, and outputs that to the screen.

---
+++ Root.java
@@ -0,0 +1,8 @@
+class Root
+{
+    public static void main(String[] args)
+    {
+        int number = 225; // Integer value of 225
+        System.out.println("The square root of " + number + " is " + Math.sqrt(number));
+    }
+} // Class Root end
