757d8792c18374ec07409029f5d3a9c39b6f441d
SentientSam <samueltlamb1997@gmail.com>
2020-04-16 11:19:57 -0500

    Add files via upload

---
+++ Saluton.java
@@ -0,0 +1,8 @@
+class Saluton // class statement, gives the program the name "Saluton"
+{ // Start of class block
+    public static void main(String[] args)
+    {
+        String greeting = "Hej Verden!"; // Attaching the string Hello World! (in Danish) to the variable greeting.
+        System.out.println(greeting); // Printing out the greeting variable       
+    }
+} // end of class Saluton
